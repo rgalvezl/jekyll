b4cec10d38be34844e785ea0494eea548f6b6ca9
root <root@ip-172-31-10-6.eu-west-1.compute.internal>
2019-05-15 09:27:41 +0000

    update script

--- runjekyll.sh
+++ runjekyll.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
 ls contino
 if [ $? -eq 0 ] 
 then
 sudo rm -Rf contino;
 else echo "does not exit";
 fi
-sudo jekyll new contino;
-sleep 5
-cd contino/
-sudo nohup bundle exec jekyll  serve --host=0.0.0.0 &
+sudo jekyll new contino
+sudo cd contino/
+sudo nohup bundle exec jekyll serve --host=0.0.0.0 &
